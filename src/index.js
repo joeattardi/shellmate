848043b98210bf9742543fc46d6a331da500145b
Joe Attardi <jattardi@gmail.com>
2025-03-05 20:34:25 -0500

    Improve output

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { program } from './cli.js';
-import { generateCommand } from './ai/index.js';
+import { program } from "./cli.js";
+import { generateCommand } from "./ai/index.js";
+import chalk from 'chalk';
 
 export async function start() {
-    const [description] = program.args;
+  const [description] = program.args;
 
-    const command = await generateCommand(description);
-    console.log(command);
+  console.log("Generating a command for you:\n");
+  console.log(chalk.bold(description));
+  const command = await generateCommand(description);
+  console.log('\nHere\'s your command:\n');
+  console.log(chalk.bold(command));
 }
